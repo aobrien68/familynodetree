59f09d7b7ec700a8b3cae0a6d21063f2a1dcac49
Aidan O'Brien <68aobrien@gmail.com>
2025-06-07 17:22:51 -0400

    getentty fix

--- fnt.c
+++ fnt.c
@@ -9,8 +9,9 @@ typedef struct {
     short children[12];
 } member_entry;
 
-member_entry getEntry(int id, FILE* stream) {
+member_entry getEntry(short id, FILE* stream) {
     member_entry result;
+    fseek(stream, (long)id * sizeof(member_entry), SEEK_SET);
     fread(&result, 1, sizeof(member_entry), stream);
     return result;
 }
