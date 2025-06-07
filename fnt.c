4c9b35fffb9582c07b288e8111938c39ce66766d
Aidan O'Brien <68aobrien@gmail.com>
2025-06-07 17:58:35 -0400

    set/addentry

--- fnt.c
+++ fnt.c
@@ -1,17 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef unsigned short eid_t;
+
 typedef struct {
     char name[32];
-    short id;
-    short partner;
-    short parents[2];
-    short children[12];
+    eid_t id;
+    eid_t partner;
+    eid_t parents[2];
+    eid_t children[12];
 } member_entry;
 
-member_entry getEntry(short id, FILE* stream) {
+member_entry getEntry(eid_t id, FILE* stream) {
     member_entry result;
-    fseek(stream, (long)id * sizeof(member_entry), SEEK_SET);
+    fseek(stream, (long)id * sizeof(member_entry), SEEK_SET + sizeof(id_t));
     fread(&result, 1, sizeof(member_entry), stream);
     return result;
+}
+
+void setEntry(eid_t id, member_entry entry, FILE* stream) {
+    fseek(stream, (long)id * sizeof(member_entry), SEEK_SET + sizeof(id_t));
+    fwrite(&entry, 1, sizeof(member_entry), stream);
+}
+
+eid_t add_entry(member_entry entry, FILE* stream) {
+    eid_t id;
+    fseek(stream, 0l, SEEK_SET);
+    fread(&id, 1, sizeof(id_t), stream);
+    id++;
+    fseek(stream, 0l, SEEK_SET);
+    fwrite(&id, 1, sizeof(id_t), stream);
+    fseek(stream, (long)id * sizeof(member_entry), SEEK_SET + sizeof(id_t));
+    fwrite(&entry, 1, sizeof(member_entry), stream);
+    return id;
 }
