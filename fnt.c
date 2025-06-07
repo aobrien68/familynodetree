fa2e6ef2c222efad144c7cf4a8ec08b8a739d368
Aidan O'Brien <68aobrien@gmail.com>
2025-06-07 17:06:00 -0400

    getentry

--- fnt.c
+++ fnt.c
@@ -3,7 +3,14 @@
 
 typedef struct {
     char name[32];
-    short ref[16];
-} entry;
+    short id;
+    short partner;
+    short parents[2];
+    short children[12];
+} member_entry;
 
-
+member_entry getEntry(int id, FILE* stream) {
+    member_entry result;
+    fread(&result, 1, sizeof(member_entry), stream);
+    return result;
+}
